ad7bd3927c23658f7b085020aa61ef55ffb40cac
Paul Tagliamonte <paultag@gmail.com>
2015-09-13 20:19:45 -0400

    python-opensource

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name="opensource",
+    version="0.1",
+    packages=[
+        'opensource',
+    ],
+    author="Paul Tagliamonte",
+    author_email="paultag@opensource.org",
+    long_description=long_description,
+    description='n/a',
+    license="GPL-3.0+",
+    url="",
+    platforms=['any']
+)
