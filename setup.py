a7911c28710a0a34d9e33f1b070ff04b0ca70ee1
Paul Tagliamonte <tag@pault.ag>
2016-04-27 22:22:16 -0400

    Add in a Description

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
 
-long_description = ""
+long_description = """
+API Wrapper to access the Open Source License API
+
+The Open Source API can be found at https://api.opensource.org, and
+some (very brief!) documentation can be found in the API server documentation
+(https://github.com/OpenSourceOrg/api/blob/master/doc/endpoints.md).
+"""
 
 setup(
     name="opensource",
-    version="0.1",
+    version="1.0",
     packages=[
         'opensource',
+        'opensource.licenses',
     ],
     author="Paul Tagliamonte",
     author_email="paultag@opensource.org",
